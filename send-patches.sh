1125a16c9e1f77f4bbfbcea9a648502faa29a2a3
Cosmin Tanislav <demonsingur@gmail.com>
2025-10-24 16:26:20 +0300

    Install dtschema automatically

--- send-patches.sh
+++ send-patches.sh
@@ -45,7 +45,12 @@ FILES=$(git diff --name-only $COMMITS)
 SCRIPT_PATH=$(realpath "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 
+if [[ ! -d "$SCRIPT_DIR/venv" ]]; then
+	python3 -m venv "$SCRIPT_DIR/venv"
+fi
+
 . "$SCRIPT_DIR/venv/bin/activate"
+pip install --upgrade dtschema
 
 while read FILE; do
 	case "$FILE" in
