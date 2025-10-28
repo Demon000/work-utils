bdb00a762b8fdc5a7bc2926fe9bd53d21045ffb2
Cosmin Tanislav <demonsingur@gmail.com>
2025-10-28 14:42:13 +0200

    Check all device tree bindings at once

--- check-patches.sh
+++ check-patches.sh
@@ -37,6 +37,7 @@ fi
 . "$SCRIPT_DIR/venv/bin/activate"
 pip install --upgrade dtschema > /dev/null 2>&1
 
+DT_SCHEMA_FILES=()
 while read FILE; do
 	case "$FILE" in
 	*.yaml)
@@ -44,10 +45,14 @@ while read FILE; do
 		if [[ "$RELATIVE_YAML" = "$FILE" ]]; then
 			echo "$FILE not under Documentation/devicetree/bindings/, skip check..."
 		else
-			echo "Testing devicetree binding $FILE"
-			"$SCRIPT_DIR"/build.sh "$BOARD" dt_binding_check DT_SCHEMA_FILES="$RELATIVE_YAML"
-			"$SCRIPT_DIR"/build.sh "$BOARD" dtbs_check DT_SCHEMA_FILES="$RELATIVE_YAML"
+			DT_SCHEMA_FILES+=("$RELATIVE_YAML")
 		fi
 		;;
 	esac
 done <<< "$FILES"
+
+echo "Testing devicetree bindings ${DT_SCHEMA_FILES[*]}"
+DT_SCHEMA_FILES_STR=$(IFS=: ; echo "${DT_SCHEMA_FILES[*]}")
+
+"$SCRIPT_DIR"/build.sh "$BOARD" dt_binding_check DT_SCHEMA_FILES="$DT_SCHEMA_FILES_STR"
+"$SCRIPT_DIR"/build.sh "$BOARD" dtbs_check DT_SCHEMA_FILES="$DT_SCHEMA_FILES_STR"
