6a9633ef05becfe1104a5542adcff284a756976d
Cosmin Tanislav <demonsingur@gmail.com>
2025-11-10 22:58:10 +0200

    fixup! Add script to install commit msg hook

--- install_commit_msg_hook.sh
+++ install_commit_msg_hook.sh
@@ -4,4 +4,4 @@ SCRIPT_PATH=$(realpath "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 
 cp "$SCRIPT_DIR/commit-msg" .git/hooks/commit-msg
-chomod u+x .git/hooks/commit-msg
+chmod u+x .git/hooks/commit-msg
